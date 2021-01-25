49dd6edb37d840f41309ec09529c28b5366c5002
Khyati Vaishnav <khyati.vaishnav.15@gmail.com>
2021-01-25 21:33:19 +0530

    Brackets are resolved perfectly
    Brackets are resolved perfectly -  reshuffling is left
    Input : Test Case 7: (((a NEAR2 b) NEAR c) NEAR2 d)
    Output: "[[a ~2 b] ~1 c] ~2 d" 
    
    Input : Test Case1:( (java NEAR2 collection)  NEAR framework) OR  (java ADJ3 collection) AND (java NEAR3 multithreading)
    Output: "[ java ~2 collection]  ~1 framework" OR  "java ~>3 collection" AND "java ~3 multithreading"

---
+++ Assignment1.java
@@ -0,0 +1,176 @@
+
+package com.relecura.assignments;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Stack;
+
+/**
+ * @author Khyati
+ * For any given string as follows you need to transfer it in the given format.
+ * Input Query => ((java NEAR2 collection NEAR framework) OR (java ADJ3 collection) AND (java NEAR3 multithreading))
+ * Output => (("[java collection]~2 framework"~1) OR "java collection"~>3 AND "java multithreading"~3)
+ * The solution should be able to handle any kind of input queries.
+ * NEAR/ADJ are two operators which will be changed to ~/~> respectively followed by the number given. e.g. ~2/~>1  -done
+ * The AND/OR operator should be unchanged. -done
+ * The open and close parentheses should be handled and checked for proper opening and closing.
+ * Few more input and output examples for reference.
+ * Input => (((a NEAR2 b) NEAR c) NEAR2 (d AND e))
+ * Output => "[[a b]~2 c]~1 (d e)"~2
+ * Input => (((a AND b) OR c) OR (d AND e))
+ * Output => (((a AND b) OR c) OR (d AND e)).   
+ * Note that the output is unchanged as it does not contains NEAR/ADJ  -done
+ * The structure and hierarchy of the query can vary as per user input.
+ * 
+ * Input Query => ((java NEAR2 collection ) NEAR framework) OR (java ADJ3 collection) AND (java NEAR3 multithreading))
+ * Output => (("[java collection]~2 framework"~1) OR "java collection"~>3 AND "java multithreading"~3)
+ * 
+ * Some transformations : (a NEAR2 b) =>  "a b"~2
+                          ((a NEAR2 b) NEAR c) => "[a b]~2 c"~1
+                          (((a NEAR2 b) NEAR c) NEAR2 d) => "[[a b]~2 c]~1 d"~2
+ */
+public class Assignment1 {
+	final static String constantNEAR = "NEAR";
+	final static String constantADJ = "ADJ";
+	final static String constantNEAR_SYMBOL = "~";
+	final static String constantADJ_SYMBOL = "~>";
+	public static void main(String[] args) {
+
+		System.out.println("--------------");
+		System.out.println("Test Case1:");
+		String input1 = "( (java NEAR2 collection)  NEAR framework) OR  (java ADJ3 collection) AND (java NEAR3 multithreading)";
+	   getOutputOngivenConditions(input1);
+		
+//		System.out.println("--------------");
+//		System.out.println("Test Case 5:");
+//		String input1 = "( (java NEAR2 collection)  NEAR framework) ";
+//	   getOutputOngivenConditions(input1);
+//		
+//	   System.out.println("--------------");
+//		System.out.println("Test Case2:");
+//		String input0 = "(a NEAR3 b)";
+//		getOutputOngivenConditions(input0);
+		
+//		System.out.println("--------------");
+//		System.out.println("Test Case3:Unchanged Output");
+//		String input2 = "(((a AND b) OR c) OR (d AND e))";
+//		getOutputOngivenConditions(input2);
+	   
+//		System.out.println("--------------");
+//		System.out.println("Test Case 6:");
+//		String input6 = "((a NEAR2 b) NEAR c)";
+//	    getOutputOngivenConditions(input6);
+		
+		System.out.println("--------------");
+		System.out.println("Test Case 7: (((a NEAR2 b) NEAR c) NEAR2 d)");
+		String input7  = "(((a NEAR2 b) NEAR c) NEAR2 d)"; //"[[a b]~2 c]~1 d"~2
+	    getOutputOngivenConditions(input7);
+	   
+	}
+	 public static void getOutputOngivenConditions(String input1) {
+		
+		 String orgStr = input1;
+		 if(input1.contains("NEAR") ||  input1.contains("ADJ") ) {
+			 String replacedStr = replaceNEARandADJ(input1);
+			 String replacedBracketsStr = replacingStringWithSquareBraces(replacedStr);
+
+			 System.out.println(replacedBracketsStr);
+
+		 }else {
+			 System.out.println(orgStr);
+		 }
+	 }
+	
+	 
+	 public static String replacingStringWithSquareBraces(String str) {
+		 Stack<String> stack = new Stack<String>();
+		 boolean flag= false;
+		 while(str.contains("(")) {
+			 String chkOpenBraces = str; 
+			 while(chkOpenBraces.contains("(")) {
+				 stack.push("(");
+				 chkOpenBraces =  chkOpenBraces.substring(chkOpenBraces.indexOf("(")+1,chkOpenBraces.contains(")")?chkOpenBraces.indexOf(")")+1:chkOpenBraces.length());
+			 }
+			  String tempBegin =str.substring(0,str.indexOf("("));
+			 String tempStr1  = str.substring(str.indexOf(")")+1, str.length());
+ 		  	 String  tempStr = str.substring(str.indexOf("(")+1, str.contains(")")?str.indexOf(")")+1:str.length());
+			 String replaceStr = null;
+						
+			 if(!stack.isEmpty() ) {
+				 if(tempStr.contains(")")) {
+					 flag = stack.pop().equalsIgnoreCase("(") ? true:false;
+					 if(flag && !stack.isEmpty()) {
+						 replaceStr =  tempStr.replace("(", "");			
+						 replaceStr = replaceStr.replace(")", "");
+						 replaceStr = "["+replaceStr+"]";
+					 }else if(flag) {
+						 replaceStr =  tempStr.replace("(", "");			
+						 replaceStr = replaceStr.replace(")", "");
+						 replaceStr = "\""+replaceStr+"\"";
+					 }
+								 
+				 }
+			 }
+			 String tempOpenBraces = "";
+			 while(!stack.isEmpty()) {
+				 tempOpenBraces = tempOpenBraces+stack.pop();
+			 }
+			 str = tempBegin+tempOpenBraces+(replaceStr!=null && !replaceStr.equalsIgnoreCase("")? replaceStr+tempStr1:tempStr1);
+		 }
+		 return  str;
+	 }
+	 
+	 
+	 public static String replaceNEARandADJ(String st) {
+		 StringBuffer sb = new StringBuffer();
+		 String tempSplit[] = st.split(" ");
+		 for (String string : tempSplit) {
+			if(string.contains(constantNEAR)) {
+				String splitNear[] = string.split(constantNEAR);
+				String count = splitNear.length==0? "1":"";
+			
+				String replaceNEAR  = string.replace(constantNEAR, "~"); 
+				sb.append(replaceNEAR+count+" ");
+			}else if(string.contains(constantADJ)) {
+				String splitNear[] = string.split(constantADJ);
+				String count = splitNear.length==0? "1":"";
+			
+				String replaceNEAR  = string.replace(constantADJ, "~>"); 
+				sb.append(replaceNEAR+count+" ");
+			}else {
+				sb.append(string+" ");
+			}
+		}
+		 return sb.toString();
+	 }
+	
+	public static String reShufflingTheString(String st) {		 
+		String[]  strArr = st.split("");
+		 Stack<String> stackTemp = new Stack<String>();
+		 StringBuffer sbBuffer = new StringBuffer();
+		 for (int i = 0; i < strArr.length; i++) {
+			 if (strArr[i].equalsIgnoreCase("~") &&  strArr[i+1].equalsIgnoreCase(">") ) {
+				 stackTemp.push(strArr[i+2]);
+				 stackTemp.push(strArr[i+1]); 
+				 stackTemp.push(strArr[i]); 
+				i+=2;
+			}else  if(strArr[i].equalsIgnoreCase("~") ) { 
+				 stackTemp.push(strArr[i+1]);
+				 stackTemp.push(strArr[i]); 
+				i++;
+			 } else {
+				 sbBuffer.append(strArr[i]);
+			 }
+		 }
+		 if(!stackTemp.isEmpty()) {
+			 sbBuffer.toString().trim();
+			 while(!stackTemp.isEmpty()) {
+				 sbBuffer.append(stackTemp.pop());
+			 }
+		 }
+		 return sbBuffer.toString();
+	}
+}
